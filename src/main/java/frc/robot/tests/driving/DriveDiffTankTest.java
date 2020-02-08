830c7e7b2311185375eac0b1f03e4c19d9b1b457
Cindy Ho <hoc.20@stu.materdei.org>
2020-02-08 13:00:46 -0800

    DiffDriveTankTest WIP

--- src/main/java/frc/robot/tests/driving/DriveDiffTankTest.java
+++ src/main/java/frc/robot/tests/driving/DriveDiffTankTest.java
@@ -4,7 +4,6 @@ package frc.robot.tests.driving;
 import edu.wpi.first.wpilibj.Timer;
 
 import frc.robot.consoles.Logger;
-import frc.robot.oi.controllers.DPadButton.Direction;
 import frc.robot.tests.TestRunnable;
 import frc.robot.VirtualControllers;
 
@@ -16,21 +15,66 @@ public class DriveDiffTankTest implements TestRunnable {
     private double m_currentTime;
 
     // Constants
-    private final double TOTAL_SECONDS = 12.0;
+    private final double TOTAL_SECONDS = 18.0;
+    private final double TOTAL_MOVEMENTS = 12.0;
+    private final double SECONDS_PER_MOVEMENT = TOTAL_SECONDS / TOTAL_MOVEMENTS;
 
     // Run the test
     public int run(int iteration) {
         if (iteration == 0) {
             // Start the test on the first iteration
             Logger.setup("Starting DriveDiffTankTest for " + TOTAL_SECONDS + " seconds...");
-            m_currentTime = 0.0;
-            m_timer.stop();
+
             m_timer.reset();
             m_timer.start();
-            VirtualControllers.primary.xbox.dpadActive = true;
-            VirtualControllers.primary.xbox.dpadDirection = Direction.UP;
 
-        } else {
+            m_currentTime = m_timer.get();
+
+            VirtualControllers.primary.xbox.xLeft = -0.5;
+            VirtualControllers.primary.xbox.yLeft = -0.5;
+        }
+        else if (m_currentTime < 1 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xLeft = 0.5;
+            VirtualControllers.primary.xbox.yLeft = 0.5;
+
+        }
+        else if (m_currentTime < 2 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xRight = -0.5;
+            VirtualControllers.primary.xbox.yRight = -0.5;
+        }
+        else if (m_currentTime < 3 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xRight = 0.5;
+            VirtualControllers.primary.xbox.yRight = 0.5;
+        }
+        else if (m_currentTime < 4 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xRight = -0.5;
+            VirtualControllers.primary.xbox.yLeft = 0.5;
+        }
+        else if (m_currentTime < 6 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xLeft = -1.0;
+            while (VirtualControllers.primary.xbox.xLeft < 1.0 && VirtualControllers.primary.xbox.xLeft >= -1.0) {
+                VirtualControllers.primary.xbox.xLeft += 0.1;
+            }
+        }
+        else if (m_currentTime < 8 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.xRight = -1.0;
+            while (VirtualControllers.primary.xbox.xRight < 1.0 && VirtualControllers.primary.xbox.xRight >= -1.0) {
+                VirtualControllers.primary.xbox.xRight += 0.1;
+            }
+        }
+        else if (m_currentTime < 10 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.yLeft = -1.0;
+            while (VirtualControllers.primary.xbox.yLeft < 1.0 && VirtualControllers.primary.xbox.yLeft >= -1.0) {
+                VirtualControllers.primary.xbox.yLeft += 0.1;
+            }
+        }
+        else if (m_currentTime < 12 * SECONDS_PER_MOVEMENT) {
+            VirtualControllers.primary.xbox.yRight = -1.0;
+            while (VirtualControllers.primary.xbox.yRight < 1.0 && VirtualControllers.primary.xbox.yRight >= -1.0) {
+                VirtualControllers.primary.xbox.yRight += 0.1;
+            }
+        }
+        else {
             // Reset the time and return 0 when all movements are complete
             Logger.ending("Ending DriveDiffTankTest...");
             m_currentTime = 0.0;
