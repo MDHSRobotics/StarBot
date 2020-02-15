7136eb8c181ab1c41c7da2ce88e4156015aaab50
Henryvu <vuh1.20@stu.materdei.org>
2020-02-15 09:21:03 -0800

    Updated Climb

---
+++ src/main/java/frc/robot/commands/climblegsred/StopRedLegs.java
@@ -0,0 +1,49 @@
+
+package frc.robot.commands.climblegsred;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.consoles.Logger;
+import frc.robot.subsystems.ClimbLegsRedLine;
+
+// This command stops the climb legs.
+public class StopSparkLegs extends CommandBase {
+
+    private ClimbLegsSpark m_climbLegsSpark;
+
+    public StopSparkLegs(ClimbLegsSpark climbLegsSpark) {
+        Logger.setup("Constructing Command: StopSparkLegs...");
+
+        // Add given subsystem requirements
+        m_climbLegsSpark = climbLegsSpark;
+        addRequirements(m_climbLegsSpark);
+    }
+
+    @Override
+    public void initialize() {
+        Logger.action("Initializing Command: StopSparkLegs...");
+    }
+
+    @Override
+    public void execute() {
+        m_climbLegsSpark.stop();
+    }
+
+    // This command continues until interrupted
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            System.out.println("--");
+            Logger.ending("Interrupting Command: StopSparkLegs...");
+        } else {
+            Logger.ending("Ending Command: StopSparkLegs...");
+        }
+        m_climbLegsSpark.stop();
+    }
+
+}
