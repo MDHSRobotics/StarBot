b02094126501c8559fa4ae7da8db3c57ef188845
megan <fjunanto@gmail.com>
2020-02-22 14:45:55 -0800

    fixed driveAlign method

--- src/main/java/frc/robot/subsystems/DiffDriver.java
+++ src/main/java/frc/robot/subsystems/DiffDriver.java
@@ -95,8 +95,8 @@ public class DiffDriver extends SubsystemBase {
             if (0 > zRotation && zRotation > -alignZSpeedMinimum) zRotation = -alignZSpeedMinimum;
         }
 
+        diffDrive.arcadeDrive(0, zRotation*2);
         Logger.action("DiffDriver -> Drive Tank: " + zRotation);
-        diffDrive.arcadeDrive(0, zRotation);
     }
 
     // TODO: Use this to indicate to the driver that the robot is aligned with the target (lights? Shuffleboard?)
