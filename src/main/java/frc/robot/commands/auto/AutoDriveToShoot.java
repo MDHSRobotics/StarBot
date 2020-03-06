bb67826398513f5dc5e73ba37d90f8b68ab72487
Cindy Ho <hoc.20@stu.materdei.org>
2020-03-05 18:24:52 -0800

    updated AutoDriveToShoot

--- src/main/java/frc/robot/commands/auto/AutoDriveToShoot.java
+++ src/main/java/frc/robot/commands/auto/AutoDriveToShoot.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj.Timer;
-import frc.robot.BotSensors;
 import frc.robot.consoles.Logger;
 import frc.robot.sensors.Limelight;
 import frc.robot.subsystems.DiffDriver;
@@ -11,7 +9,6 @@ import frc.robot.subsystems.DiffDriver;
 public class AutoDriveToShoot extends CommandBase {
 
     private DiffDriver m_diffDriver;
-    private Limelight m_Limelight;
 
     private static final double TARGET_YAW = 0.0;
     private static final double DISTANCE_X_FROM_TARGET = 3.05;
@@ -29,7 +26,7 @@ public class AutoDriveToShoot extends CommandBase {
     public void initialize() {
         Logger.action("Initializing Command: AutoDriveToShoot...");
 
-        currentXDistanceFromTarget = BotSensors.m_Limelight.getXOffset();
+        currentXDistanceFromTarget = Limelight.getXOffset();
     }
 
     @Override
