088eb29f02f7fc9f30f35747d7edcb221ccd69a5
Cindy Ho <hoc.20@stu.materdei.org>
2020-03-05 17:48:55 -0800

    updated autoDriveFromPickUp

--- src/main/java/frc/robot/commands/auto/AutoDriveFromPickUp.java
+++ src/main/java/frc/robot/commands/auto/AutoDriveFromPickUp.java
@@ -1,7 +1,7 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.BotSensors;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.consoles.Logger;
 import frc.robot.subsystems.DiffDriver;
 
@@ -10,10 +10,12 @@ public class AutoDriveFromPickUp extends CommandBase {
 
     private DiffDriver m_diffDriver;
 
-    private static boolean isTargetReached = false;
+    private Timer m_timer = new Timer();
+    private double m_timeLastPrinted = 0.0;
 
-    private static double targetMin = 3.0;
-    private static double targetMax = 3.05;
+    private static final double MAX_DRIVE_SECONDS = 3.5;
+
+    private static final double TARGET_YAW = 0.0;
 
     public AutoDriveFromPickUp(DiffDriver diffDriver) {
         Logger.setup("Constructing Command: AutoDriveFromPickUp...");
@@ -26,17 +28,35 @@ public class AutoDriveFromPickUp extends CommandBase {
     @Override
     public void initialize() {
         Logger.action("Initializing Command: AutoDriveFromPickUp...");
+
+        m_timer.reset();
+        m_timer.start();
+
     }
 
     @Override
     public void execute() {
-        isTargetReached = m_diffDriver.driveToWithinRange(BotSensors.distanceSensorFront, targetMin, targetMax);
+        double currentTime = m_timer.get();
+        double timeElapsedSincePrint = currentTime - m_timeLastPrinted;
+
+        if (timeElapsedSincePrint > 1.0) {
+            Logger.action("AutoDriveForward: -> Moved Forward for " + currentTime);
+            m_timeLastPrinted = currentTime;
+        }
+        m_diffDriver.driveAlign(TARGET_YAW);
     }
 
     // This command continues until it target is reached
     @Override
     public boolean isFinished() {
-        return isTargetReached;
+        double currentTime = m_timer.get();
+
+        if (currentTime < MAX_DRIVE_SECONDS) {
+            return false;
+        } else {
+            Logger.action("AutoDriveFromPickUp: -> Stopped");
+            return true;
+        }
     }
 
     @Override
