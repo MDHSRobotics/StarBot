ff893c271073433093a703ee57a20708aade8f87
Paul Sicking <sickingp@gmail.com>
2020-03-07 22:52:42 -0700

    Lookup table for determining motor speed of shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,8 @@
 
 package frc.robot.subsystems;
 
+import java.util.*;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -114,6 +116,57 @@ public class Shooter extends SubsystemBase {
         talonSrxShooterTopWheel.set(ControlMode.Velocity, nativeVelocity, DemandType.AuxPID, heading);
     }
 
+    // Translate a desired target velocity in feet per second to a motor speed in Ticks per 100 ms.
+    // The translation is done via a lookup table with values based on shooting experiments.
+    // Each entry in the table is the result of testing a particular motor speed (Ticks per 100ms) and
+    // then measuring the range that the ball is shot. From the range we can calculate the effective
+    // velocity of the ball (ft/sec). The lookup table correlates the ball velocity and motor speed
+    // for each test.  We can deduce the motor speed for other ball velocities by interpolating
+    // (or extrapolating) using the values in the lookup table.
+
+    public static double translateFPSToTicksViaTable(double targetFPS) {
+
+        // Initialize the lookup table; Key=Velocity in FPS; Value=Motor speed in Ticks/100ms
+        SortedMap<Double, Double> luTable = new TreeMap<Double, Double>();
+
+        // The data below is based on shooting experiments conducted on March 5, 2020:
+        // (Feet per second, Ticks per 100ms)
+        luTable.put(20., 200.);
+        luTable.put(30., 300.);
+        luTable.put(50., 500.);
+        luTable.put(90., 1700.);
+
+        boolean firstPass = true;
+        double f1 = -99.;
+        double t1 = -99.;
+        double targetTPHMS = -99.;
+
+        // Iterate over the lookup table, which is sorted based on the key (fps)
+        for (Map.Entry<Double, Double> entry : luTable.entrySet()) {
+            double f2 = entry.getKey();
+            double t2 = entry.getValue();
+            System.out.println(f2 + " => " + t2);
+
+            // Skip over the first value because we can't compute a slope until we have read at least 2 values
+            if (firstPass) {
+                firstPass = false;
+            }
+            else {
+                double slope = (t2-t1) / (f2-f1);
+                targetTPHMS = t1 + (targetFPS-f1) * slope;
+
+                if (targetFPS <= f2)
+                    break;
+            }
+
+            f1 = f2;
+            t1 = t2;
+        }
+
+        return targetTPHMS;
+    }
+
+
     // Adjust the shooter velocity based on the distance determined by values set in Shuffleboard
     public void shootWithDistance() {
         double sHeight = RobotBrain.shooterHeightFeetDefault;
@@ -124,7 +177,9 @@ public class Shooter extends SubsystemBase {
         double numerator = 32.2 * Math.pow(distanceFeet, 2);
         double denominator = 2 * (distanceFeet * Math.sin(sAngle) * Math.cos(sAngle) - (fHeight - sHeight) * Math.pow(Math.cos(sAngle), 2));
         double velocityFPS = Math.sqrt(numerator) / Math.sqrt(denominator);
-        double nativeVelocity = EncoderUtils.translateFPSToTicksPerDecisecond(velocityFPS, WHEEL_DIAMETER, GEAR_RATIO) / 10;
+
+        // Convert the desired ball velocity (ft/sec) into the required motor speed (Ticks per 100 ms)
+        double nativeVelocity = translateFPSToTicksViaTable(velocityFPS);
 
         Logger.info("Shooter DistanceVelocity-> FlyWheel Native Velocity:" + nativeVelocity + " TPDS");
         Logger.info("Shooter DistanceVelocity-> FlyWheel FPS: " + velocityFPS);
@@ -137,6 +192,7 @@ public class Shooter extends SubsystemBase {
 
         ShooterBrain.setTargetFPS(velocityFPS);
         ShooterBrain.setTargetTPHMS(nativeVelocity);
+
     }
 
     //---------//
